d15dacd8f38cfde81c7fe834274f6c297685c2ac
mohamed khaled <moham35356@gmail.com>
2022-08-23 18:30:36 +0200

    adding card and courses components

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Card.css";
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img src={props.imgsrc} alt="" />
+      <h5 className="card-title">{props.title}</h5>
+      <span className="card-author">{props.author}</span>
+      <span className="card-price">&#69;&#163;{props.price}</span>
+    </div>
+  );
+};
+export default Card;
